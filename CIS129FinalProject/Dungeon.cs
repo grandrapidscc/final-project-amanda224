2c56cbb0bfacb4847fdb3f7a9647741a954465ae
amanda224 <amandaseeley@email.grcc.edu>
2022-12-05 23:22:29 -0500

    Design a Wizert Game
    The program is not totally complete, it has some bug problem.

--- CIS129FinalProject/Dungeon.cs
+++ CIS129FinalProject/Dungeon.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
@@ -20,220 +21,65 @@ namespace CIS129FinalProject
                  GoblinAttackBodySlam, OrcAttackCleave,
                  BansheeAttackScreech)
         {
-            var location = new Random();
 
-            var GoblinLocations = new List<int>();
-            // Goblin has 5 locations
-            for (int i = 0; i <= 4; i++)
-            {
-                GoblinLocations.Add(location.Next(0, 25));
-            }
-
-            var OrcLocation = new List<int>();
-            // Orc 5 has locations
-            for (int j = 0; j <= 4; j++)
-            {
-                OrcLocation.Add(location.Next(0, 25));
-            }
-
-            var BansheeLocation = new List<int>();
-            // Banshee has 5 locations
-            for (int k = 0; k <= 4; k++)
-            {
-                BansheeLocation.Add(location.Next(0, 25));
-            }
-
-            var WizertLocation = new List<int>();
-            // Wizert has 15 locations
-            for (int n = 0; n <= 14; n++)
-            {
-                WizertLocation.Add(location.Next(0, 25));
-            }
-
-            var HealthPotionLocation = new List<int>();
-            // Health Potion has 5 locations
-            for (int k = 0; k <= 4; k++)
-            {
-                HealthPotionLocation.Add(location.Next(0, 25));
-            }
-
-            var MagickaPotionLocation = new List<int>();
-            // Magicka Potion has 5 locations
-            for (int k = 0; k <= 4; k++)
-            {
-                MagickaPotionLocation.Add(location.Next(0, 25));
-            }
-
-            while (hpWizert >= 0)
-            {
-                // attacking Orc
-                for (int k = 0; k <= WizertLocation.Count; k++)
-                {
-                    for (int j = 0; j <= OrcLocation.Count; j++)
-                    {
-                        if (k == j)
-                        {
-                            Console.WriteLine("You have encountered a Orc");
-                            Console.WriteLine($"It's the current HP {hpOrc}.");
-                            Console.WriteLine("Press...\n" +
-                                "1.To Attack\n" +
-                                "2.To Heal\n" +
-                                "3.To Attempt to Flee");
-                            Console.WriteLine();
-                            string option = Console.ReadLine();
-                            if (option == "1")
-                            {
-                                while (hpOrc >= 0)
-                                {
-                                    hpWizert -= 3;
-                                    mpWizert -= 3;
-                                    hpOrc -= 5;
-                                }
-                                Console.WriteLine("Attend Fireball attack\nIt had damage Orc 5 Hp ");
-                                Console.WriteLine($"You have defeated Orc\nWizert current Health is {hpWizert}");
-                            }
-                            else if (option == "2")
-                            {
-                                mpWizert -= 5;
-                                hpWizert += 3;
-                                Console.WriteLine($"Wizert current Hp {hpWizert} and current Mp {mpWizert}");
-
-                            }
-                            else if (option == "3")
-                            {
-                                Console.WriteLine("Pick an option\n1.North\n2.South\n3.West\n4.East");
-                                string direction = Console.ReadLine();
-                                if (direction == "1" || direction == "2" || direction == "3" || direction == "4")
-                                {
-                                    WizertLocation[0] = WizertLocation[0] - 1;
-                                }
-                            }
-
-
-                        }
-                    }
-                }
-                // atttacking Goblin
-                for (int k = 0; k <= WizertLocation.Count; k++)
-                {
-                    for (int j = 0; j <= GoblinLocations.Count; j++)
-                    {
-                        if (k == j)
-                        {
-                            Console.WriteLine("You have encountered a Goblin");
-                            Console.WriteLine($"It's the current HP {hpGoblin}.");
-                            Console.WriteLine("Press...\n" +
-                                "1.To Attack\n" +
-                                "2.To Heal\n" +
-                                "3.To Attempt to Flee");
-                            Console.WriteLine();
-                            string option = Console.ReadLine();
-                            if (option == "1")
-                            {
-                                while (hpGoblin >= 0)
-                                {
-                                    hpWizert -= 2;  // damage by Goblin
-                                    mpWizert -= 3;  // cause 3 Mp to attack Fireball
-                                    hpGoblin -= 5;
-                                }
-                                Console.WriteLine("Attend Fireball attack\nIt had damage Goblin 3 Hp ");
-                                Console.WriteLine($"You have defeated Goblin\nWizert current Health is {hpWizert}");
-                            }
-                            else if (option == "2")
-                            {
-                                mpWizert -= 5;
-                                hpWizert += 3;
-                                Console.WriteLine($"Wizert current Hp {hpWizert} and current Mp {mpWizert}");
-
-                            }
-                            else if (option == "3")
-                            {
-                                Console.WriteLine("Pick an option\n1.North\n2.South\n3.West\n4.East");
-                                string direction = Console.ReadLine();
-                                if (direction == "1" || direction == "2" || direction == "3" || direction == "4")
-                                {
-                                    WizertLocation[0] = WizertLocation[0] - 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                // attacking Banshee 
-                for (int k = 0; k <= WizertLocation.Count; k++)
-                {
-                    for (int j = 0; j <= BansheeLocation.Count; j++)
-                    {
-                        if (k == j)
-                        {
-                            Console.WriteLine("You have encountered a Banshee");
-                            Console.WriteLine($"It's the current HP {hpBanshee}.");
-                            Console.WriteLine("Press...\n" +
-                                "1.To Attack\n" +
-                                "2.To Heal\n" +
-                                "3.To Attempt to Flee");
-                            Console.WriteLine();
-                            string option = Console.ReadLine();
-                            if (option == "1")
-                            {
-                                while (hpBanshee >= 0)
-                                {
-                                    hpWizert -= 5;  // damage by Banshee
-                                    mpWizert -= 3;  // cause 3 Mp to attack Fireball
-                                    hpBanshee -= 5;
-                                }
-                                Console.WriteLine("Attend Fireball attack\nIt had damage Banshee 8 Hp ");
-                                Console.WriteLine($"You have defeated Banshee\nWizert current Health is {hpWizert}\n");
-                            }
-                            else if (option == "2")
-                            {
-                                mpWizert -= 5;
-                                hpWizert += 3;
-                                Console.WriteLine($"Wizert current Hp {hpWizert} and current Mp {mpWizert}");
-
-                            }
-                            else if (option == "3")
-                            {
-                                Console.WriteLine("Pick an option\n1.North\n2.South\n3.West\n4.East");
-                                string direction = Console.ReadLine();
-                                if (direction == "1" || direction == "2" || direction == "3" || direction == "4")
-                                {
-                                    WizertLocation[0] = WizertLocation[0] - 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                // Wizert and Health Potion at the same location
-                for (int k = 0; k <= WizertLocation.Count; k++)
-                {
-                    for (int j = 0; j <= HealthPotionLocation.Count; j++)
-                    {
-                        if (k == j)
-                        {
-                            hpWizert += 10;
-                        }
-                    }
-                }
-                // Wizert and Magicka Potion at the same location
-                for (int k = 0; k <= WizertLocation.Count; k++)
-                {
-                    for (int j = 0; j <= GoblinLocations.Count; j++)
-                    {
-                        if (k == j)
-                        {
-                            mpWizert += 20;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("Wizert have defeated. Will you like to play again?\n press 'y' to play again or press any key to exit. ");
-            string userInput = Console.ReadLine();
-            while (userInput == "y")
-            {
-                hpWizert = 100;
-                mpWizert = 200;
-            }
-            Console.WriteLine("Thank you for playing Wizert 5");
         }
+        public static bool reachExitLocation()
+        {
+            bool result = false;
+            if (Wizert.playerLocationX == 5 && Wizert.playerLocationY == 5)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static bool encounterGoblin()
+        {
+            bool result = false;
+            if (Enemy.GoblinLocation() == Wizert.playerLocation())
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static bool encounterBanshee()
+        {
+            bool result = false;
+            if (Enemy.BansheeLocation() == Wizert.playerLocation())
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static bool encounterOrc()
+        {
+            bool result = false;
+            if (Enemy.OrcLocation() == Wizert.playerLocation())
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        
+        public static bool seeHealthPotion()
+        {
+            bool result = false;
+            if (Wizert.HealthPotionLocation() == Wizert.playerLocation())
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static bool seeMagickaPotion()
+        {
+            bool result = false;
+            if (Wizert.MagickaPotionLocation() == Wizert.playerLocation())
+            {
+                result = true;
+            }
+            return result;
+        }
+
+
     }
 }
--- CIS129FinalProject/Enemy.cs
+++ CIS129FinalProject/Enemy.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace CIS129FinalProject
 {
-    internal class Enemy
+    public class Enemy
     {
-        private int _hpGoblin;
-        private int _hpOrc;
-        private int _hpBanshee;
+        public static int _hpGoblin = 3;
+        public static int _hpOrc = 5;
+        public static int _hpBanshee = 8;
 
-        private int _GoblinAttackBodySlam;
-        private int _OrcAttackCleave;
-        private int _BansheeAttackScreech;
+        public static int _GoblinAttackBodySlam = 2;
+        public static int _OrcAttackCleave = 3;
+        public static int _BansheeAttackScreech = 5;
 
         public Enemy(int hpGoblin, int hpOrc, int hpBanshee, int GoblinAttackBodySlam, int OrcAttackCleave, int BansheeAttackScreech)
         {
@@ -25,6 +25,115 @@ namespace CIS129FinalProject
             _OrcAttackCleave = OrcAttackCleave;
             _BansheeAttackScreech = BansheeAttackScreech;
         }
+        public static int GoblinLocationX { get; set; }
+        public static int GoblinLocationY { get; set; }
+        public static int OrcLocationX { get; set; }
+        public static int OrcLocationY { get; set; }
+        public static int BansheeLocationX { get; set; }
+        public static int BansheeLocationY { get; set; }
 
+        public int setGoblinLocationX()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                GoblinLocationX = enemyLocation[i];
+            }
+            return GoblinLocationX;
+        }
+        public int setGoblinLocationY()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                GoblinLocationY = enemyLocation[i];
+            }
+            return GoblinLocationY;
+        }
+        public int setOrcLoctionX()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                OrcLocationX = enemyLocation[i];
+            }
+            return OrcLocationX;
+        }
+        public int setOrcLOcationY()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                OrcLocationY = enemyLocation[i];
+            }
+            return OrcLocationY;
+        }
+        public int setBansheeLocationX()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                BansheeLocationX = enemyLocation[i];
+            }
+            return BansheeLocationX;
+        }
+        public int setBansheeLocationY()
+        {
+            var location = new Random();
+            var enemyLocation = new List<int>();
+
+            for (int n = 0; n <= 5; n++)
+            {
+                enemyLocation.Add(location.Next(0, 25));
+            }
+            for (int i = 0; i < enemyLocation.Count; i++)
+            {
+                BansheeLocationY = enemyLocation[i];
+            }
+            return BansheeLocationY;
+        }
+
+        public static (int, int) GoblinLocation()
+        {
+            return (GoblinLocationX , GoblinLocationY);
+        }
+        public static (int, int) BansheeLocation()
+        {
+            return (BansheeLocationX, BansheeLocationY);
+        }
+        public static (int, int) OrcLocation()
+        {
+            return (OrcLocationX, OrcLocationY);
+        }
     }
 }
